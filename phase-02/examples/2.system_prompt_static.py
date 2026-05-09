4f8c03c6607a1f89ffa5476538a809d65f15b036
rajendra <rajendra.venkata@hotmail.com>
2026-05-09 04:27:56 +0000

    code changes

--- phase-02/examples/2.system_prompt_static.py
+++ phase-02/examples/2.system_prompt_static.py
@@ -2,27 +2,24 @@ from langchain.agents import create_agent
 from langchain.tools import tool 
 
 @tool 
-def calculate(expression: str) -> str :
-    """
-        Args: 
-            expression: evaluate the methematical expession in string format.
-        Result:
-            Returns. the value/result of the expression  
-    """
-
-    try:
-        result = eval(expression)
-        return f"Result: {result}"
-    except Exception as e:
-        return f"Error: {e}"
+def calculate(expression: str) -> str: 
+    """Evaluate a mathematical expression."""
+    try: 
+        result - eval(expression)
+        return "result: {result}"
+    except Exception as e: 
+        return "Error {e}"
 
 agent = create_agent(
     model = "claude-haiku-4-5-20251001",
     tools = [calculate],
-    system_prompt= "You are a helpful math tutor. when user asks for a calculation, use the calculator tool. Explain the steps Clearly."
+    system_prompt="You are a helpful math tutor, when a user ask for calculations, use the calculate tool. Explain the steps clearly."
 )
 
 result = agent.invoke({
-    "messages": [{"role":"user","content":"Calculate 2 + 2 * 3"}]
+    "messages":[
+        {"role":"user","content":"Calculate 2 + 2 * 3"}
+    ]
 })
+
 print(result)
