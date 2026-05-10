5eb9d6fcf96a27392476c4d11d35de0af3bce582
rajendra <rajendra.venkata@hotmail.com>
2026-05-10 14:14:37 +0000

    code changes

--- phase-02/examples/16.multi_modal_messages.py
+++ phase-02/examples/16.multi_modal_messages.py
@@ -17,9 +17,9 @@ print(response.text)
 # Different image types
 message_with_types = HumanMessage(content_blocks=[
     {"type": "text", "text": "Analyze these:"},
-    {"type": "image", "url": "https://example.com/chart.png", "mime_type": "image/png"},
+    {"type": "image", "url": "https://img.magnific.com/free-vector/red-arrow-going-up-with-bar-chart_1308-110320.jpg?semt=ais_hybrid&w=740&q=80", "mime_type": "image/png"},
     {"type": "image", "url": "https://example.com/diagram.jpg", "mime_type": "image/jpeg"}
 ])
 
-response = model.invoke([message_with_types])
-print(response.text)
+#response = model.invoke([message_with_types])
+#print(response.text)
